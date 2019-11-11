2d419b763173a9df87749bae2175be0c3142f2c9
FirmanJabar <firmanabduljabar@gmail.com>
2019-11-11 08:10:37 +0800

    add struktur dasar html

--- pages/content/1/editor/1/live_editor.js
+++ pages/content/1/editor/1/live_editor.js
@@ -13,72 +13,17 @@ function setupEditor() {
 	editor.getSession().setMode("ace/mode/html");
 	editor.setValue(`<!DOCTYPE html>
 <html>
-
 <head>
-	<style>
-		table {
-			font-family: arial, sans-serif;
-			border-collapse: collapse;
-			width: 100%;
-		}
 
-		td,
-		th {
-			border: 1px solid #dddddd;
-			text-align: left;
-			padding: 8px;
-		}
-
-		tr:nth-child(even) {
-			background-color: #dddddd;
-		}
-	</style>
+	<title>Page Title</title>
+	
 </head>
-
 <body>
 
-	<h2>HTML Table</h2>
-
-	<table>
-		<tr>
-			<th>Company</th>
-			<th>Contact</th>
-			<th>Country</th>
-		</tr>
-		<tr>
-			<td>Alfreds Futterkiste</td>
-			<td>Maria Anders</td>
-			<td>Germany</td>
-		</tr>
-		<tr>
-			<td>Centro comercial Moctezuma</td>
-			<td>Francisco Chang</td>
-			<td>Mexico</td>
-		</tr>
-		<tr>
-			<td>Ernst Handel</td>
-			<td>Roland Mendel</td>
-			<td>Austria</td>
-		</tr>
-		<tr>
-			<td>Island Trading</td>
-			<td>Helen Bennett</td>
-			<td>UK</td>
-		</tr>
-		<tr>
-			<td>Laughing Bacchus Winecellars</td>
-			<td>Yoshi Tannamuri</td>
-			<td>Canada</td>
-		</tr>
-		<tr>
-			<td>Magazzini Alimentari Riuniti</td>
-			<td>Giovanni Rovelli</td>
-			<td>Italy</td>
-		</tr>
-	</table>
+	<h1>My First Heading</h1>
+	<p>My first paragraph.</p>
 
 </body>
-
 </html>`, 1); //1 = moves cursor to end
 
 	editor.getSession().on('change', function () {
